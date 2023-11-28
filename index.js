956ca6036040909cd532eb8b034a67f19e298b4a
lakphy <lakphycn@gmail.com>
2023-11-28 18:03:40 +0800

    init

--- index.js
+++
@@ -1,5 +0,0 @@
-function helloWorld() {
-  console.log("Hello World from this npm package");
-}
-
-module.exports = helloWorld;
---
+++ lib/cjs/types/index.d.ts
@@ -0,0 +1,3 @@
+declare function helloWorld(): void;
+export default helloWorld;
+//# sourceMappingURL=index.d.ts.map
