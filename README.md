bced2a948075fa1593b4a637862275fcb6749d9f
lakphy <lakphycn@gmail.com>
2023-11-28 17:28:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Font Slice Webpack Plugin
+
+It's my first time to write a webpack plugin. I hope it can help you.
---
+++ index.js
@@ -0,0 +1,5 @@
+function helloWorld() {
+  console.log("Hello World from this npm package");
+}
+
+module.exports = helloWorld;
